8bbe46132f208bf193df933c9dba8552eceb3082
chuxiaofeng <chuxiaofeng@meicai.cn>
2015-10-04 22:12:12 +0800

    add example

--- AopClass.php
+++ AopClass.php
@@ -7,43 +7,73 @@
  * 3. 不支持静态方法的aop，不支持private方法的aop
  *
  * TODO：通过参数控制支持私有变量与方法的AOP（是否需要？）
+ * 通配符情况下，Advice的参数有问题
+ * 新建一个AopPoint类，统一Advice传入的参数
  *
  * @authre xiaofeng
  */
 class AopClass {
-	const TYPE_BEFORE = 1;		// 方法或属性读写钱
-	const TYPE_AFTER = 2;		// 方法或属性读写后
-	const TYPE_EXCEPTION = 3;	// 方法执行触发异常
+	const TYPE_BEFORE 	= 1;		// 方法或属性读写钱
+	const TYPE_AFTER 	= 2;		// 方法或属性读写后
+	const TYPE_AROUND 	= 3;		// 替换原方法
+	const TYPE_EXCEPTION= 4;		// 方法执行触发异常
 
 	const R = 0b100; // public属性写
 	const W = 0b10;  // public属性写
 	const X = 0b1;   // public方法执行
 
+	public $className;	// 原对象名或实例
 	public $refClass;	// Aop类的反射对象
 	public $refObject;	// Aop类的实例
 	public $events;		// Aop事件列表
 	public $classProxy;	// Aop类的代理类
 
 	public function __construct($className) {
-		$this->refClass = new ReflectionClass($className);
-		$this->classProxy = new ClassProxy($this);
+		$this->calssName = $className;
+		if(is_object($className)) {
+			$this->refClass = new ReflectionClass($className);
+			$this->classProxy = new ClassProxy($this);
+		} else if(is_string($className) && class_exists($className)){
+			$this->refClass = new ReflectionClass($className);
+			$this->classProxy = new ClassProxy($this);
+		} else {
+			throw new InvalidArgumentException("must be object or className");
+		}
 	}
 
 	/**
 	 * 通过invoke得到传入类的代理类
+	 * 如果通过已经实例化的类构造的代理，则返回clone的对象的代理对象
+	 * 否则，实例化新对象的代理类
 	 * @param 原有类构造函数参数
 	 * @return Object
 	 */
 	public function __invoke() {
-		$this->refObject = $this->refClass->newInstanceArgs(func_get_args());
+		if(is_object($this->className)) {
+			$this->refObject = clone $className;
+		} else {
+			$this->refObject = $this->refClass->newInstanceArgs(func_get_args());
+		}
 		return $this->classProxy;
+	}
+
+	/**
+	 * 方法名与属性名匹配
+	 * @param  string $pattern 模式
+	 * @param  string $name    属性名与方法名
+	 * @return bool
+	 */
+	protected function match($pattern, $name) {
+		// return preg_match($pattern, $name);
+		// 正则替换成通配符的方式
+		return fnmatch($pattern, $name);
 	}
 
 	/**
 	 * 添加Advice
 	 * @param int   	$type    Aop类型
 	 * @param int   	$rwx     RWX（RW属性读写，X方法执行）
-	 * @param string   	$pattern 方法或属性名称（支持通配符*）
+	 * @param string   	$pattern 方法或属性名称（支持fnmatch）
 	 * @param callable 	$advice  Advice
 	 */
 	public function addAdvice($type, $rwx, $pattern, callable $advice) {
@@ -82,8 +112,7 @@ class AopClass {
 		$ret = [];
 		$patterns = $this->events[$type][$rwx];
 		foreach($patterns as $pattern => $advices) {
-			// fixme 不用preg_match用通配符的方式
-			if(preg_match($pattern, $name)) {
+			if($this->match($pattern, $name)) {
 				$ret = array_merge($ret, $advices);
 			}
 		}
@@ -101,10 +130,65 @@ class ClassProxy {
 	private $_;
 
 	public function __construct(AopClass $aopClass) {
-		if(!$aopClass->refClass->hasProperty('_')) {
+		if($aopClass->refClass->hasProperty('_')) {
 			trigger_error('$_ property has been used');
 		}
 		$this->_ = $aopClass;
+	}
+
+	/**
+	 * 正常流程与around流程异常处理
+	 * @param  Exception $e
+	 * @param  string    $name 方法名
+	 * @param  &mixed    &$ret 返回值
+	 * @throws Exception
+	 */
+	private function exceptionHandler(Exception $e, $name, &$ret) {
+		$aop = $this->_;
+		$advices = $aop->getAdvices(AopClass::TYPE_EXCEPTION, AopClass::X, $name);
+		if($advices) {
+			// 参数通用引用传递，发生异常时可以修改返回值
+			// advice方法签名参数必须为引用
+			array_walk($advices, function($advice) use($e, &$ret) {
+				$advice($e, $ret);
+			});
+		} else {
+			// 异常类型信息丢失
+			throw $e;
+		}
+	}
+
+	/**
+	 * 包装原方法,用于around
+	 * @param  string $name 方法名
+	 * @param  array  $args 方法参数数组
+	 * @return closure
+	 */
+	private function methodWrapper($name, array $args) {
+		return function() use($args, $name){
+			return call_user_func_array([$this->_->refObject, $name], $args);
+		};
+	}
+
+	/**
+	 * 调用原方法
+	 * @param  [type] $name [description]
+	 * @return [type]       [description]
+	 */
+	private function call($name, array $args) {
+		$obj = $this->_->refObject;
+		// return call_user_func_array([$obj, $name], $args);
+
+		// 替换成反射方式invoke
+		$refMethod = new ReflectionMethod($this->_->refObject, $name);
+		if(!$refMethod->isPublic()) {
+			throw new BadMethodCallException("method $name is not public");
+		}
+		if($refMethod->getNumberOfParameters()) {
+			return $refMethod->invoke($obj);
+		} else {
+			return $refMethod->invokeArgs($obj, $args);
+		}
 	}
 
 	/**
@@ -118,6 +202,7 @@ class ClassProxy {
 		// 默认调用方只调用原对象方法
 		$aop = $this->_;
 
+		// BEFORE
 		$advices = $aop->getAdvices(AopClass::TYPE_BEFORE, AopClass::X, $name);
 		// 参数通用引用传递，请求前可以修改参数
 		// advice方法签名参数必须为引用
@@ -125,19 +210,32 @@ class ClassProxy {
 			$advice($args);
 		});
 
-		// private 问题
+		// call 返回值
 		$ret = null;
-		try  {
-			$ret = call_user_func_array([$aop->refObject, $name], $args);
-		} catch(Exception $e) {
-			$advices = $aop->getAdvices(AopClass::TYPE_EXCEPTION, AopClass::X, $name);
-			// 参数通用引用传递，发生异常时可以修改返回值
-			// advice方法签名参数必须为引用
-			array_walk($advices, function($advice) use($e, &$ret) {
-				$advice($e, $ret);
-			});
+
+		$advices = $aop->getAdvices(AopClass::TYPE_AROUND, AopClass::X, $name);
+		if($advices) {
+			// AROUND
+			try {
+				// advice 接受三个参数
+				// 1.原方法传入参数，可修改, 2.旧方法closure, 3.around返回值carry
+				array_walk($advices, function($advice) use(&$args, &$ret, $name) {
+					$ret = $advice($args, $this->methodWrapper($name, $args), $ret);
+				});
+			} catch(Exception $e) {
+				$this->exceptionHandler($e, $name, $ret);
+			}
+		} else {
+			// EXCEPTION
+			try  {
+				// $ret = $this->call($name, $args);
+				$ret = call_user_func_array([$aop->refObject, $name], $args);
+			} catch(Exception $e) {
+				$this->exceptionHandler($e, $name, $ret);
+			}
 		}
 
+		// AFTER
 		$advices = $aop->getAdvices(AopClass::TYPE_AFTER, AopClass::X, $name);
 		// 参数通用引用传递，请求后可以修改返回值
 		// advice方法签名参数必须为引用
@@ -156,6 +254,7 @@ class ClassProxy {
 	public function __set($name, $value) {
 		$aop = $this->_;
 
+		// BEFORE
 		$advices = $aop->getAdvices(AopClass::TYPE_BEFORE, AopClass::W, $name);
 		// 参数通用引用传递，赋值前可修改值
 		// advice方法签名参数必须为引用
@@ -164,9 +263,10 @@ class ClassProxy {
 			$advice($value);
 		});
 
-		// private 问题
+		// SET
 		$aop->refObject->$name = $value;
 
+		// AFTER
 		$advices = $aop->getAdvices(AopClass::TYPE_AFTER, AopClass::W, $name);
 		array_walk($advices, function($advice) use($value) {
 			// FIXME class line file info
@@ -183,14 +283,16 @@ class ClassProxy {
 	public function __get($name) {
 		$aop = $this->_;
 
+		// BEFORE
 		$advices = $aop->getAdvices(AopClass::TYPE_BEFORE, AopClass::R, $name);
 		array_walk($advices, function($advice) {
 			$advice();
 		});
 
-		// private 问题
+		// GET
 		$ret = $aop->refObject->$name;
 
+		// AFTER
 		$advices = $aop->getAdvices(AopClass::TYPE_AFTER, AopClass::R, $name);
 		// 参数通用引用传递，读取之后可修改返回值
 		// advice方法签名参数必须为引用
--- Example.php
+++ Example.php
@@ -17,51 +17,78 @@ class A {
 
 	public function throw() {
 		throw new Exception("Error Processing Request", 1);
+		return 'normal';
+	}
+
+	public function around($arg) {
+		// throw new Exception("Error Processing Request", 1);
+		return $arg;
 	}
 }
 
-$Aop = new AopClass('A');
+$oldA = new A(M_PI);
+// 接受对象或类名
+$Aop = new AopClass($oldA);
+// $Aop = new AopClass('A');
+
 // 请求前修改参数，参数必须写引用
-$Aop->addAdvice(AopClass::TYPE_BEFORE, AopClass::X, '/func/', function(&$args) {
+$Aop->addAdvice(AopClass::TYPE_BEFORE, AopClass::X, 'func', function(&$args) {
 	$args[0] = 'im-return';
 	echo "before call\n";
 });
 // 请求后修改返回值，参数必须写引用
-$Aop->addAdvice(AopClass::TYPE_AFTER, AopClass::X, '/func/', function(&$ret) {
+$Aop->addAdvice(AopClass::TYPE_AFTER, AopClass::X, 'func', function(&$ret) {
 	$ret .= "_1";
 	echo "after-call-1\n";
 });
-$Aop->addAdvice(AopClass::TYPE_AFTER, AopClass::X, '/func/', function(&$ret) {
+$Aop->addAdvice(AopClass::TYPE_AFTER, AopClass::X, 'func', function(&$ret) {
 	$ret .= "_2";
 	echo "after-call-2\n";
 });
 // 执行异常时触发
-$Aop->addAdvice(AopClass::TYPE_EXCEPTION, AopClass::X, '/throw/', function($e, &$ret) {
-	echo 'exception: ' . $e->getMessage();
+$Aop->addAdvice(AopClass::TYPE_EXCEPTION, AopClass::X, 'throw', function($e, &$ret) {
+	echo 'exception: ' . $e->getMessage() . PHP_EOL;
+	// 异常发生后修改返回值
+	$ret = 'exceptionResult';
 });
+// 执行异常时触发
+$Aop->addAdvice(AopClass::TYPE_EXCEPTION, AopClass::X, 'around', function($e, &$ret) {
+	echo 'exception: ' . $e->getMessage() . PHP_EOL;
+	// 异常发生后修改返回值
+	$ret = 'exceptionResult';
+});
+// 替换原方法
+$Aop->addAdvice(AopClass::TYPE_AROUND, AopClass::X, 'around', function($args, callable $process, &$ret){
+	return $process() . '_around1_';
+});
+$Aop->addAdvice(AopClass::TYPE_AROUND, AopClass::X, 'around', function($args, callable $process, &$ret){
+	return $ret . '_around2_';
+});
+
 // 赋值前可修改，参数必须写引用
-$Aop->addAdvice(AopClass::TYPE_BEFORE, AopClass::W, '/property/', function(&$val) {
+$Aop->addAdvice(AopClass::TYPE_BEFORE, AopClass::W, 'property', function(&$val) {
 	$val = 'BEFORE_' . $val;
 	echo "set property $val\n";
 });
-$Aop->addAdvice(AopClass::TYPE_AFTER, AopClass::W, '/property/', function() {
+$Aop->addAdvice(AopClass::TYPE_AFTER, AopClass::W, 'property', function() {
 	echo "set property finished\n";
 });
-$Aop->addAdvice(AopClass::TYPE_BEFORE, AopClass::R, '/property/', function() {
+$Aop->addAdvice(AopClass::TYPE_BEFORE, AopClass::R, 'property', function() {
 	echo "get property\n";
 });
 // 读取之后课修改返回值，参数必须写引用
-$Aop->addAdvice(AopClass::TYPE_AFTER, AopClass::R, '/property/', function(&$val) {
+$Aop->addAdvice(AopClass::TYPE_AFTER, AopClass::R, 'property', function(&$val) {
 	$val = 'Modified_' . $val;
 	echo "get property finished\n";
 });
 
 
 $A = $Aop("hello");
-echo $A->func('ret') . "\n";
+echo $A->func('ret') . PHP_EOL;
 $A->property = "world";
-echo $A->property . "\n";
-$A->throw();
+echo $A->property . PHP_EOL;
+echo $A->throw() . PHP_EOL;
+echo $A->around('test-around') . PHP_EOL;
 
 // out
 // before call
